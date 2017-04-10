4bc810f861a17df920e90bc09d6dbb19464262d8
MD RASHEDUL ALAM <smrashedcse@gmail.com>
2017-04-10 12:30:48 +0600

    Phone With Addess book in Android
    android Address Book with Phone

---
+++ AddressBook/app/src/main/java/com/p_soft/p_soft/addressbook/MainActivity.java
@@ -0,0 +1,84 @@
+package com.p_soft.p_soft.addressbook;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "MESSAGE";
+    private ListView obj;
+    SqliteHelper mydb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mydb = new SqliteHelper(this);
+        ArrayList array_list = mydb.getAllCotacts();
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, array_list);
+
+        obj = (ListView)findViewById(R.id.listView1);
+        obj.setAdapter(arrayAdapter);
+        obj.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+
+                int id_To_Search = arg2 + 1;
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+
+                Intent intent = new Intent(getApplicationContext(),DisplayContact.class);
+
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()) {
+            case R.id.item1:Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", 0);
+
+                Intent intent = new Intent(getApplicationContext(),DisplayContact.class);
+                intent.putExtras(dataBundle);
+
+                startActivity(intent);
+                return true;
+
+            case R.id.item2:
+                Intent i=new Intent(getApplicationContext(),about.class);
+                startActivity(i);
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean onKeyDown(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true);
+        }
+        return super.onKeyDown(keycode, event);
+    }
+}
---
+++ AddressBook/app/src/main/java/com/p_soft/p_soft/addressbook/about.java
@@ -0,0 +1,16 @@
+package com.p_soft.p_soft.addressbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by USER on 4/10/2017.
+ */
+
+public class about extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
